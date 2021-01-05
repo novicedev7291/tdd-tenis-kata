596a0fd46247d9f2db9a6a091c61d01bcb3903c9
kuldeep <kuldeepyadav7291@gmail.com>
2021-01-05 23:41:12 +0530

    Remove sonar warning

--- src/main/java/org/example/tennis/EitherResult.java
+++ src/main/java/org/example/tennis/EitherResult.java
@@ -1,13 +1,16 @@
 package org.example.tennis;
 
 import io.vavr.control.Either;
+import lombok.NoArgsConstructor;
 
 import static io.vavr.control.Either.left;
 import static io.vavr.control.Either.right;
+import static lombok.AccessLevel.PRIVATE;
 
 /**
  * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
  */
+@NoArgsConstructor(access = PRIVATE)
 class EitherResult {
     static <L,R> Either<L,R> announceSuccess(R o) {
         return right(o);
--- src/main/java/org/example/tennis/WinEvent.java
+++
@@ -1,7 +0,0 @@
-package org.example.tennis;
-
-/**
- * @author <a href="kuldeepyadav7291@gmail.com">Kuldeep</a>
- */
-class WinEvent {
-}
